4a1aa03ac5c11fe4d4278a6e37e176bc441df356
sreyom31 <sreyom31@gmail.com>
2022-01-08 04:52:09 +0530

    redux thunk asynchronous get request
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 import {
     FETCH_USERS_REQUEST,
     FETCH_USERS_SUCCESS,
@@ -21,5 +23,22 @@ export const fetchUsersFailure = error => {
     return {
         type: FETCH_USERS_FAILURE,
         payload: error
+    }
+}
+
+export const fetchUsers = () => {
+    return dispatch => {
+        dispatch(fetchUsersRequest())
+        axios
+            .get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                // response.data is the users
+                const users = response.data
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(error => {
+                // error.message is the error message
+                dispatch(fetchUsersFailure(error.message))
+            })
     }
 }
